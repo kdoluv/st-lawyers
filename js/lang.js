38e63ef68f9b2533c749d13cb6b472c7d321a240
مكتب المحاميان سعد الرومي وطارق السبيعي <info@st-lawyers.com>
2026-01-15 12:21:04 +0300

    Update lang.js
    Signed-off-by: مكتب المحاميان سعد الرومي وطارق السبيعي <info@st-lawyers.com>

--- js/lang.js
+++ js/lang.js
@@ -2,8 +2,8 @@ function setLang(lang) {
   document.documentElement.lang = lang;
   document.documentElement.dir = (lang === "ar") ? "rtl" : "ltr";
 
-  document.querySelectorAll("[data-ar]").forEach(el => {
-    el.textContent = el.getAttribute(`data-${lang}`);
-  });
+document.querySelectorAll("input[data-ar], textarea[data-ar]").forEach(el => {
+  el.placeholder = el.getAttribute(`data-${lang}`);
+});
 }
 
