9bcf918d8d008f2abe73cb05cb578273e08ee846
مكتب المحاميان سعد الرومي وطارق السبيعي <info@st-lawyers.com>
2026-02-01 09:36:23 +0300

    Update mobile-menu.js
    Signed-off-by: مكتب المحاميان سعد الرومي وطارق السبيعي <info@st-lawyers.com>

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,7 +3,4 @@ function toggleMenu() {
   document.getElementById("mainNav").classList.toggle("open");
 }
 
-function toggleMenu() {
-  document.querySelector('.nav').classList.toggle('open');
-}
 </script>
